d741880c3e84e98853721af3352a80067b0d5075
virn90 <40902887+virn90@users.noreply.github.com>
2020-08-29 05:07:09 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,72 +1,42 @@
-const CACHE_NAME = 'my-cache v1';
+// This is the service worker with the Cache-first network
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(cache =>
-                cache.addAll([
-                    'favicon.ico',
-                    'projects.json',
-                    'style.css',
-                    'index.js',
-                    'https://fonts.googleapis.com/css?family=Open+Sans:400,700'
-                ])
-            )
-    );
+const CACHE = "pwabuilder-precachev2";
+const precacheFiles = [
+  /* Add an array of files to precache for your app */
+  
+];
+
+self.addEventListener("install", function (event) {
+  console.log("[PWA Builder] Install Event processing");
+
+  console.log("[PWA Builder] Skip waiting on install");
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+    })
+  );
 });
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            if (response) {
-                //found cached resource
-                return response;
-            }
-            return fetch(event.request);
-        })
-    );
+
+// Allow sw to control of current page
+self.addEventListener("activate", function (event) {
+  console.log("[PWA Builder] Claiming clients for current page");
+  event.waitUntil(self.clients.claim());
 });
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            if (response) {
-                //found cached resource
-                return response;
-            }
 
-            // get resource and add it to cache
-            return fetch(event.request)
-                .then(response => {
-                    // check if the response is valid
-                    if (!response.ok) {
-                        return response;
-                    }
-
-                    // clone the response
-                    const newResponse = response.clone();
-
-                    // add it to cache
-                    caches.open(CACHE_NAME)
-                        .then(cache =>
-                            cache.put(event.request, newResponse)
-                        );
-
-                    // return response
-                    return response;
-                });
-        })
-    );
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener("fetch", function (event) {
+ 
 });
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.map(cache => {
-                    if (cache === CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
-    );
-});
+
+function fromCache(request) {
+}
+
+function updateCache(request, response) {
+}
+// This is the "Offline page" service worker
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "offline.html";
